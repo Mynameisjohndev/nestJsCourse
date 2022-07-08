8c7b658c50cb76c04e5678f5df72efb179c891ff
Myanameisjohndev <joaoantonio15161516@gmail.com>
2022-07-07 21:34:03 -0300

    Working with request params

--- devtraining/src/courses/courses.controller.ts
+++ devtraining/src/courses/courses.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
-  @Get('list')
+  @Get()
   findAll() {
     return 'Listagem de cursos';
+  }
+
+  @Get(':id')
+  finOnde(@Param('id') id: string) {
+    return `Curso número #${id}`;
   }
 }
